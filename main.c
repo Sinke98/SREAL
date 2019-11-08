81f536ba833a0ae69f5c061c8e9128f46beb42e4
Sinke98 <41339087+Sinke98@users.noreply.github.com>
2019-11-08 11:33:43 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -11,9 +11,11 @@ int main(){
   FILE *f_ledovke;
   char *str;
   char pr1;
+  char pritisnut1 = 0;
+  char pritisnut2 = 0;
   char t0, t1, t2, t3;
   size_t br_bajtova = 6;
-  float procenat=0;
+  float procenat = 0;
   long int perioda = 20000L;
 
   // Glavni deo
@@ -38,7 +40,7 @@ int main(){
       return -1;
     }
 
-    pr1 = str[2] - 48;
+    pr1 = str[2] - '0';
 
     free(str);
 
@@ -60,10 +62,10 @@ int main(){
       return -1;
     }
 
-    t0 = str[2] - 48;
-    t1 = str[3] - 48;
-    t2 = str[4] - 48;
-    t3 = str[5] - 48;  
+    t0 = str[2] - '0';
+    t1 = str[3] - '0';
+    t2 = str[4] - '0';
+    t3 = str[5] - '0';  
     
     free(str);
 
@@ -71,138 +73,71 @@ int main(){
 
     // Ako je prekidac "SW0" aktivan
     if(pr1 == 1){
-      // Ako je taster "BTN0" aktivan
-      if(t0 == 1){
 
-        procenat += 0.2;
+      f_ledovke = fopen("/dev/led", "w");
 
-        // Upali ledovke
-
-        f_ledovke = fopen("/dev/led","w");
-
-        if(f_ledovke == NULL){
-          puts("Problem pri otvaranju /dev/led");
-          return -1;
-        }
-
-        fputs("0xf\n",f_ledovke);
-
-        if(fclose(f_ledovke)){
-          puts("Problem pri zatvaranju /dev/led");
-          return -1;
-        }
-
-        usleep(procenat*perioda);
-
-        // Ugasi ledovke
-
-        f_ledovke = fopen("/dev/led","w");
-
-        if(f_ledovke == NULL){
-          puts("Problem pri otvaranju /dev/led");
-          return -1;
-        }
-
-        fputs("0x0\n",f_ledovke);
-
-        if(fclose(f_ledovke)){
-          puts("Problem pri zatvaranju /dev/led");
-          return -1;
-        }
-
-        usleep((1-procenat)*perioda);  
-
+      if(f_ledovke == NULL){
+        puts("Problem pri otvaranju /dev/led");
+        return -1;
       }
-      // Ako je taster "BTN1" aktivan
-      if(t1 == 1){
 
-        procenat -= 0.2;
+      fputs("0xf\n",f_ledovke);
 
-        // Upali ledovke
-
-        f_ledovke = fopen("/dev/led", "w");
-
-        if(f_ledovke == NULL){
-          puts("Problem pri otvaranju /dev/led");
-          return -1;
-        }
-
-        fputs("0xf\n",f_ledovke);
-
-        if(fclose(f_ledovke)){
-          puts("Problem pri zatvaranju /dev/led");
-          return -1;
-        }
-
-        usleep(procenat*perioda);
-
-        // Ugasi ledovke
-
-        f_ledovke = fopen("/dev/led","w");
-
-        if(f_ledovke == NULL){
-          puts("Problem pri otvaranju /dev/led");
-          return -1;
-        }
+      // Ako je taster "BTN0" aktivan
+      if(t0 == 1 && pritisnut1 == 0){
         
-        fputs("0x0\n",f_ledovke);
+        pritisnut1 = 1;
 
-        if(fclose(f_ledovke)){
-          puts("Problem pri zatvaranju /dev/led");
-          return -1;
+        if(pritisnut1 == 1){
+
+          // Povecanje faktora ispune za 20%
+          procenat += 0.2;
         }
+      }  
+      // Ako je taster "BTN1" aktivan
+      if(t1 == 1 && pritisnut2 == 0){
+        
+        pritisnut2 = 1;
 
-        usleep((1-procenat)*perioda);
+        if(pritisnut2 == 1){
 
+          // Smanjenje faktora ispune za 20%
+          procenat -= 0.2;
+        }
       }
       // Ako je pritisnut taster "BTN2"
       if(t2 == 1){
-      
-        procenat = 0;
-
+     
         // Postavi faktor ispune na 0%
-    
-        f_ledovke = fopen("/dev/led","w");
-
-        if(f_ledovke == NULL){
-          puts("Problem pri otvaranju /dev/led");
-          return -1;
-        }
-
-        fputs("0x0\n",f_ledovke);
-
-        if(fclose(f_ledovke)){
-          puts("Problem pri zatvaranju /dev/led");
-          return -1;
-        }
-
-        usleep((1-procenat)*perioda);
-
+        procenat = 0;
       }
       // Ako je taster "BTN3" aktivan
       if(t3 == 1){
-        
-        procenat = 1;
-
+       
         // Postavi faktor ispune na 100%
-        
-        f_ledovke = fopen("/dev/led","w");
-
-        if(f_ledovke == NULL){
-          puts("Problem pri otvaranju /dev/led");
-          return -1;
-        }
-
-        fputs("0xf",f_ledovke);
-
-        if(fclose(f_ledovke)){
-          puts("Problem pri zatvaranju /dev/led");
-          return -1;
-        }
-
-        usleep(procenat*perioda);  
-
+        procenat = 1;
       }
+
+      usleep(procenat*perioda);
+
+      fputs("0x0\n",f_ledovke);
+
+      if(fclose(f_ledovke)){
+        puts("Problem pri zatvaranju /dev/led");
+        return -1;
+      }
+
+      usleep((1-procenat)*perioda);
+
+      if(t0 == 0){
+        
+        pritisnut1 = 0;
+      }
+
+      if(t1 == 0){
+        
+        pritisnut2 = 0;
+      }  
 
     }
       
